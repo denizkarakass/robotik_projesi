13406a9936663d268faceb4547e47f32ce98eb79
Deniz Karakaş <96745481+denizkarakass@users.noreply.github.com>
2022-12-05 12:10:13 +0300

    m

---
+++ controllers/modelController.py
@@ -0,0 +1,17 @@
+# İHA boyutlarını 2*2*2 şeklinde alıyorum.
+# Engel, İHA, İKA konumları ROS aracılığıyla sensörlerden alınacak.
+# ROS'dan MongoDB veritabanına aktarılıp güncel veriler buraya çekilebilir.
+
+from ..models import iha1Model
+from ..models import iha2Model
+from ..models import iha3Model
+from ..models import iha4Model
+from ..models import iha5Model
+from ..models import iha6Model
+
+from ..models import ika1Model
+from ..models import ika2Model
+
+
+İKHAs = [iha1Model, iha2Model, iha3Model, iha4Model, iha5Model, iha6Model, ika1Model, ika2Model]
+
--- controllers/rotation.py
+++ controllers/rotation.py
@@ -1,45 +1,37 @@
-# İHA boyutlarını 2*2*2 şeklinde alıyorum.
-# Engel, İHA, İKA konumları ROS aracılığıyla sensörlerden alınacak.
-# ROS'dan MongoDB veritabanına aktarılıp güncel veriler buraya çekilebilir.
-
-from ..models import iha1Model
-from ..models import iha2Model
-
-İHA1 = iha1Model.İHA1
-İHA2 = iha2Model.İHA2
+from modelController import İKHAs
 
 # Mesafe hesaplamaları
-xDistance = abs(İHA1["x"] - İHA2["x"])
-yDistance = abs(İHA1["y"] - İHA2["y"])
-zDistance = abs(İHA1["z"] - İHA2["z"])
+xDistance = abs(İKHAs[0]["x"] - İKHAs[1]["x"])
+yDistance = abs(İKHAs[0]["y"] - İKHAs[1]["y"])
+zDistance = abs(İKHAs[0]["z"] - İKHAs[1]["z"])
 
 print(xDistance)
 
 # Eğer çarpışma durumu riski olmazsa bunları uygulayacak.
 if xDistance > 5:
-    İHA1["x"] += 10
-    İHA2["x"] += 10
+    İKHAs[0]["x"] += 10
+    İKHAs[1]["x"] += 10
     print("10 birim ileri")
 if zDistance > 5:
-    İHA1["y"] += 10
-    İHA2["y"] += 10
+    İKHAs[0]["y"] += 10
+    İKHAs[1]["y"] += 10
     print("10 birim çapraz")
 
 # Eğer çarpışma riski olursa bunları uygulayacak.
 if xDistance < 5:
-    İHA1["x"] += 5
-    İHA2["x"] += -5  # Aradaki mesafeyi açmak için ters yöne yönlendiritoruz.
+    İKHAs[0]["x"] += 5
+    İKHAs[1]["x"] += -5  # Aradaki mesafeyi açmak için ters yöne yönlendiritoruz.
 if yDistance < 5:
-    İHA1["y"] += 5
-    İHA2["y"] += -5
+    İKHAs[0]["y"] += 5
+    İKHAs[1]["y"] += -5
 if zDistance < 5:
-    İHA1["z"] += 5
-    İHA2["z"] += -5
+    İKHAs[0]["z"] += 5
+    İKHAs[1]["z"] += -5
 
 
 # Engele 20 birim mesafeden az kaldığı zaman kaçma hareketi yapıcak
-if İHA1["e-x"] < 20 and İHA1["e-x"] != 0:
-   İHA1["y"] += 20
+if İKHAs[0]["e-x"] < 20 and İKHAs[0]["e-x"] != 0:
+   İKHAs[0]["y"] += 20
 
-if İHA1["e-y"] < 20 and İHA1["e-y"] != 0:
-   İHA1["y"] += 20
+if İKHAs[0]["e-y"] < 20 and İKHAs[0]["e-y"] != 0:
+   İKHAs[0]["y"] += 20
