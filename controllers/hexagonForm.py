3087707d5dedc816f3c4b63e97921ae5b7fcb754
Furkan Bulut <93736726+Furk4nBulut@users.noreply.github.com>
2022-12-05 13:48:23 +0300

    m

--- controllers/hexagonForm.py
+++ controllers/hexagonForm.py
@@ -1,12 +1,25 @@
 from modelController import IKHAs
 
-
+#ihalar
 IKHAs[0]["X"] = 10
 IKHAs[1]["X"] = 10
 IKHAs[2]["X"] = 10
 IKHAs[3]["X"] = 10
 IKHAs[4]["X"] = 10
 IKHAs[5]["X"] = 10
+#ikalar
 IKHAs[6]["X"] = 10
+IKHAs[7]["X"] = 10
+
+#ihalar
+IKHAs[0]["Y"] = 10
+IKHAs[1]["Y"] = 10
+IKHAs[2]["Y"] = 10
+IKHAs[3]["Y"] = 10
+IKHAs[4]["Y"] = 10
+IKHAs[5]["Y"] = 10
+#ikalar
+IKHAs[6]["Y"] = 10
+IKHAs[7]["Y"] = 10
 
 
