6a5198a7dc4f9a34647f8fcfc77efa54dbf35321
Deniz Karakaş <96745481+denizkarakass@users.noreply.github.com>
2022-12-05 11:57:10 +0300

    m

---
+++ controllers/starForm.py
@@ -0,0 +1,7 @@
+
+from ..models import iha1Model
+from ..models import iha2Model
+
+İHA1 = iha1Model.İHA1
+İHA2 = iha2Model.İHA2
+
---
+++ models/iha3Model.py
@@ -0,0 +1,11 @@
+İHA3 = {
+    "name": "İHA-1",
+    "x": 40,  # İHA-1 x:[1], y:[2], z:[3] konumunda
+    "y": 50,
+    "z": 30,
+    # Engelin x,y,z eksenlerine olan uzaklık mesafesi ROS'dan gelen veri.
+    "e-x": 10,
+    # Eğer ortada engel yoksa ROS'dan 0 değeri dönecek ve bunu kullanıcaz.
+    "e-y": 0,
+    "e-z": 0,
+}
