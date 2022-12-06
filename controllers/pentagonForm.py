f643274783d15d024fecc03410acd9ea59c0ad00
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-06 13:05:13 +0300

    pentagonForm.py

---
+++ controllers/pentagonForm.py
@@ -0,0 +1,29 @@
+from modelController import IHAs
+from modelController import IKAs
+
+#IHAların x konumu
+IHAs[0]["x"] = 10
+IHAs[1]["x"] = 15,0
+IHAs[2]["x"] = 35,0
+IHAs[3]["x"] = 40,12
+
+#IHAların y konumu
+IHAs[0]["y"] = 100
+IHAs[1]["y"] = 100
+IHAs[2]["y"] = 100
+IHAs[3]["y"] = 100
+
+#IHAların z konumu
+IHAs[0]["z"] = 29.34
+IHAs[1]["z"] = 10
+IHAs[2]["z"] = 10
+IHAs[3]["z"] = 29.34
+
+#IKAların x konumu
+IKAs[0]["x"] = 25,
+
+#IKAların y konumu
+IKAs[0]["y"] = 0
+
+#IKAların z konumu
+IKAs[0]["z"] = 41,84
