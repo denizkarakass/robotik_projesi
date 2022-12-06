773428bf10171b64e014d5da86d34bdf42392e2a
KaraElBatson <119845319+KaraElBatson@users.noreply.github.com>
2022-12-06 12:27:39 +0300

    Create newmoonForm.py

---
+++ controllers/newmoonForm.py
@@ -0,0 +1,39 @@
+from modelController import IHAs
+from modelController import IKAs
+
+#IHAların x konumu
+IHAs[0]["x"] = 100
+IHAs[1]["x"] = 50
+IHAs[2]["x"] = 30
+IHAs[3]["x"] = 10
+IHAs[4]["x"] = 30
+IHAs[5]["x"] = 50
+IHAs[6]["x"] = 100
+#IHAların y konumu
+IHAs[0]["y"] = 100
+IHAs[1]["y"] = 100
+IHAs[2]["y"] = 100
+IHAs[3]["y"] = 100
+IHAs[4]["y"] = 100
+IHAs[5]["y"] = 100
+IHAs[6]["y"] = 100
+#IHAların z konumu
+IHAs[0]["z"] = 60
+IHAs[1]["z"] = 100
+IHAs[2]["z"] = 80
+IHAs[3]["z"] = 50
+IHAs[4]["z"] = 20
+IHAs[5]["z"] = 10
+IHAs[6]["z"] = 40
+#IKAların x konumu
+IKAs[0]["x"] = 75
+IKAs[1]["x"] = 50
+IKAs[2]["x"] = 75
+#IKAların y konumu
+IKAs[0]["y"] = 0
+IKAs[1]["y"] = 0
+IKAs[2]["y"] = 0
+#IKAların z konumu
+IKAs[0]["z"] = 70
+IKAs[1]["z"] = 50
+IKAs[2]["z"] = 30
