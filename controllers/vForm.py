5b92200d18f98249d877cebf16c5e4483d565c85
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-06 13:48:29 +0300

    v

--- controllers/vForm.py
+++ controllers/vForm.py
@@ -1,35 +1,35 @@
 from modelController import IHAs
 from modelController import IKAs
- 
+o = 10
 #IHAların x konumu
-IHAs[0]["x"] = 10
-IHAs[1]["x"] = 20
-IHAs[2]["x"] = 30
-IHAs[3]["x"] = 40
-IHAs[4]["x"] = 50
+IHAs[0]["x"] = o + 10
+IHAs[1]["x"] = o + 20
+IHAs[2]["x"] = o + 30
+IHAs[3]["x"] = o + 40
+IHAs[4]["x"] = o + 50
 
 #IHAların y konumu
-IHAs[0]["y"] = 100
-IHAs[1]["y"] = 100
-IHAs[2]["y"] = 100
-IHAs[3]["y"] = 100
-IHAs[4]["y"] = 100
+IHAs[0]["y"] = o + 100
+IHAs[1]["y"] = o + 100
+IHAs[2]["y"] = o + 100
+IHAs[3]["y"] = o + 100
+IHAs[4]["y"] = o + 100
 
 #IHAların z konumu
-IHAs[0]["z"] = 30
-IHAs[1]["z"] = 20
-IHAs[2]["z"] = 10
-IHAs[3]["z"] = 20
-IHAs[4]["z"] = 30
+IHAs[0]["z"] = o + 30
+IHAs[1]["z"] = o + 20
+IHAs[2]["z"] = o + 10
+IHAs[3]["z"] = o + 20
+IHAs[4]["z"] = o + 30
 #IKAların x konumu
-IKAs[0]["x"] = 10
-IKAs[1]["x"] = 30
-IKAs[2]["x"] = 50
+IKAs[0]["x"] = o + 10
+IKAs[1]["x"] = o + 30
+IKAs[2]["x"] = o + 50
 #IKAların y konumu
-IKAs[0]["y"] = 0
-IKAs[1]["y"] = 0
-IKAs[2]["y"] = 0
+IKAs[0]["y"] = o + 0
+IKAs[1]["y"] = o + 0
+IKAs[2]["y"] = o + 0
 #IKAların z konumu
-IKAs[0]["z"] = 30
-IKAs[1]["z"] = 10
-IKAs[2]["z"] = 30
+IKAs[0]["z"] = o + 30
+IKAs[1]["z"] = o + 10
+IKAs[2]["z"] = o + 30
