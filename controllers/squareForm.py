893e60748fb0052309dc2f47b5e1c6f536f4fd1a
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-06 13:47:28 +0300

    square

--- controllers/squareForm.py
+++ controllers/squareForm.py
@@ -1,41 +1,42 @@
 from modelController import IHAs
 from modelController import IKAs
 #IHAların x konumu
-IHAs[0]["x"] = 10
-IHAs[1]["x"] = 15
-IHAs[2]["x"] = 20
-IHAs[3]["x"] = 20
-IHAs[4]["x"] = 20
-IHAs[5]["x"] = 15
-IHAs[6]["x"] = 10
-IHAs[7]["x"] = 10
+o = 10
+IHAs[0]["x"] =  o +10
+IHAs[1]["x"] =  o +15
+IHAs[2]["x"] =  o +20
+IHAs[3]["x"] =  o +20
+IHAs[4]["x"] =  o +20
+IHAs[5]["x"] =  o +15
+IHAs[6]["x"] =  o +10
+IHAs[7]["x"] =  o +10
 #IHAların y konumu
-IHAs[0]["y"] = 100
-IHAs[1]["y"] = 100
-IHAs[2]["y"] = 100
-IHAs[3]["y"] = 100
-IHAs[4]["y"] = 100
-IHAs[5]["y"] = 100
-IHAs[6]["y"] = 100
-IHAs[7]["y"] = 100
+IHAs[0]["y"] =  100
+IHAs[1]["y"] =  100
+IHAs[2]["y"] =  100
+IHAs[3]["y"] =  100
+IHAs[4]["y"] =  100
+IHAs[5]["y"] =  100
+IHAs[6]["y"] =  100
+IHAs[7]["y"] =  100
 #IHAların z konumu
-IHAs[0]["z"] = 10
-IHAs[1]["z"] = 10
-IHAs[2]["z"] = 10
-IHAs[3]["z"] = 15
-IHAs[4]["z"] = 20
-IHAs[5]["z"] = 20
-IHAs[6]["z"] = 20
-IHAs[7]["z"] = 15
+IHAs[0]["z"] =  o +10
+IHAs[1]["z"] =  o +10
+IHAs[2]["z"] =  o +10 
+IHAs[3]["z"] =  o +15
+IHAs[4]["z"] =  o +20
+IHAs[5]["z"] =  o +20
+IHAs[6]["z"] =  o +20
+IHAs[7]["z"] =  o +15
 #IKAların x konumu
-IKAs[0]["x"] = 10
-IKAs[1]["x"] = 20
-IKAs[2]["x"] = 15
+IKAs[0]["x"] =  o +10
+IKAs[1]["x"] =  o +20
+IKAs[2]["x"] =  o +15
 #IKAların y konumu
 IKAs[0]["y"] = 0
 IKAs[1]["y"] = 0
 IKAs[2]["y"] = 0
 #IKAların z konumu
-IKAs[0]["z"] = 10
-IKAs[1]["z"] = 20
-IKAs[2]["z"] = 10
+IKAs[0]["z"] =  o +10
+IKAs[1]["z"] =  o +20
+IKAs[2]["z"] =  o +10
