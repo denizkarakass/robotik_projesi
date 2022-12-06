a51c096f0a668a9355bc6a8913d5a702e9e7e477
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-06 13:25:17 +0300

    triangleForm.py

---
+++ controllers/triangleForm.py
@@ -0,0 +1,32 @@
+from modelController import IHAs
+from modelController import IKAs
+
+#IHAların x konumu
+IHAs[0]["x"] = 10
+IHAs[1]["x"] = 40
+IHAs[2]["x"] = 10
+IHAs[3]["x"] = 10
+IHAs[4]["x"] = 40
+
+#IHAların y konumu
+IHAs[0]["y"] = 150
+IHAs[1]["y"] = 150
+IHAs[2]["y"] = 150
+IHAs[3]["y"] = 75
+IHAs[4]["y"] = 75
+
+#IHAların z konumu
+IHAs[0]["z"] = 10
+IHAs[1]["z"] = 10
+IHAs[2]["z"] = 50
+IHAs[3]["z"] = 10
+IHAs[4]["z"] = 50
+
+#IKAların x konumu
+IKAs[0]["x"] = 10
+
+#IKAların y konumu
+IKAs[0]["y"] = 0
+
+#IKAların z konumu
+IKAs[0]["z"] = 50
