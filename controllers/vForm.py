5085e58a839cc664907ec2d34229362f3578fda7
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-06 12:44:07 +0300

    vForm

---
+++ controllers/vForm.py
@@ -0,0 +1,35 @@
+from modelController import IHAs
+from modelController import IKAs
+ 
+#IHAların x konumu
+IHAs[0]["x"] = 10
+IHAs[1]["x"] = 20
+IHAs[2]["x"] = 30
+IHAs[3]["x"] = 40
+IHAs[4]["x"] = 50
+
+#IHAların y konumu
+IHAs[0]["y"] = 100
+IHAs[1]["y"] = 100
+IHAs[2]["y"] = 100
+IHAs[3]["y"] = 100
+IHAs[4]["y"] = 100
+
+#IHAların z konumu
+IHAs[0]["z"] = 30
+IHAs[1]["z"] = 20
+IHAs[2]["z"] = 10
+IHAs[3]["z"] = 20
+IHAs[4]["z"] = 30
+#IKAların x konumu
+IKAs[0]["x"] = 10
+IKAs[1]["x"] = 30
+IKAs[2]["x"] = 50
+#IKAların y konumu
+IKAs[0]["y"] = 0
+IKAs[1]["y"] = 0
+IKAs[2]["y"] = 0
+#IKAların z konumu
+IKAs[0]["z"] = 30
+IKAs[1]["z"] = 10
+IKAs[2]["z"] = 30
