7757266fd322a68b44976dae8bd12978d9bab3dc
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-06 13:50:51 +0300

    newmoon

--- controllers/newmoonFrom.py
+++ controllers/newmoonFrom.py
@@ -1,14 +1,14 @@
 from modelController import IHAs
 from modelController import IKAs
-   
+o = 10   
 #IHAların x konumu
-IHAs[0]["x"] = 100
-IHAs[1]["x"] = 50
-IHAs[2]["x"] = 30
-IHAs[3]["x"] = 10
-IHAs[4]["x"] = 30
-IHAs[5]["x"] = 50
-IHAs[6]["x"] = 100
+IHAs[0]["x"] = o + 100
+IHAs[1]["x"] = o + 50
+IHAs[2]["x"] = o + 30
+IHAs[3]["x"] = o + 10
+IHAs[4]["x"] = o + 30
+IHAs[5]["x"] = o + 50
+IHAs[6]["x"] = o + 100
 #IHAların y konumu
 IHAs[0]["y"] = 100
 IHAs[1]["y"] = 100
@@ -18,22 +18,22 @@ IHAs[4]["y"] = 100
 IHAs[5]["y"] = 100
 IHAs[6]["y"] = 100
 #IHAların z konumu
-IHAs[0]["z"] = 60
-IHAs[1]["z"] = 100
-IHAs[2]["z"] = 80
-IHAs[3]["z"] = 50
-IHAs[4]["z"] = 20
-IHAs[5]["z"] = 10
-IHAs[6]["z"] = 40
+IHAs[0]["z"] = o + 60
+IHAs[1]["z"] = o + 100
+IHAs[2]["z"] = o + 80
+IHAs[3]["z"] = o + 50
+IHAs[4]["z"] = o + 20
+IHAs[5]["z"] = o + 10
+IHAs[6]["z"] = o + 40
 #IKAların x konumu
-IKAs[0]["x"] = 75
-IKAs[1]["x"] = 50
-IKAs[2]["x"] = 75
+IKAs[0]["x"] = o + 75
+IKAs[1]["x"] = o + 50
+IKAs[2]["x"] = o + 75
 #IKAların y konumu
 IKAs[0]["y"] = 0
 IKAs[1]["y"] = 0
 IKAs[2]["y"] = 0
 #IKAların z konumu
-IKAs[0]["z"] = 70
-IKAs[1]["z"] = 50
-IKAs[2]["z"] = 30
+IKAs[0]["z"] = o + 70
+IKAs[1]["z"] = o + 50
+IKAs[2]["z"] = o + 30
