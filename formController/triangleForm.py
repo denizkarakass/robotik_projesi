5df421ea5aade252f0258fe1c38d11c5c4350379
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-07 17:39:53 +0300

    EMRE
    = yerine =+ yazdırma ve IHA,IKA sayısı belirtme

--- formController/triangleForm.py
+++ formController/triangleForm.py
@@ -1,32 +1,34 @@
 from data import IHAs
 from data import IKAs
 o = 10
+#IHA:5
+#IKA:1
 #IHAların x konumu
-IHAs[0]["x"] = o +10
-IHAs[1]["x"] = o +40
-IHAs[2]["x"] = o +10
-IHAs[3]["x"] = o +10
-IHAs[4]["x"] = o +40
+IHAs[0]["x"] =+ o +10
+IHAs[1]["x"] =+ o +40
+IHAs[2]["x"] =+ o +10
+IHAs[3]["x"] =+ o +10
+IHAs[4]["x"] =+ o +40
 
 #IHAların y konumu
-IHAs[0]["y"] = 150
-IHAs[1]["y"] = 150
-IHAs[2]["y"] = 150
-IHAs[3]["y"] = 75
-IHAs[4]["y"] = 75
+IHAs[0]["y"] =+ 150
+IHAs[1]["y"] =+ 150
+IHAs[2]["y"] =+ 150
+IHAs[3]["y"] =+ 75
+IHAs[4]["y"] =+ 75
 
 #IHAların z konumu
-IHAs[0]["z"] = o +10
-IHAs[1]["z"] = o +10
-IHAs[2]["z"] = o +50
-IHAs[3]["z"] = o +10
-IHAs[4]["z"] = o +50
+IHAs[0]["z"] =+ o +10
+IHAs[1]["z"] =+ o +10
+IHAs[2]["z"] =+ o +50
+IHAs[3]["z"] =+ o +10
+IHAs[4]["z"] =+ o +50
 
 #IKAların x konumu
-IKAs[0]["x"] = o +10
+IKAs[0]["x"] =+ o +10
 
 #IKAların y konumu
-IKAs[0]["y"] = 0
+IKAs[0]["y"] =+ 0
 
 #IKAların z konumu
-IKAs[0]["z"] = o +50
+IKAs[0]["z"] =+ o +50
