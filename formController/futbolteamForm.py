0c58cc313189a0994f426d1ca7b07807c3010923
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-07 18:39:05 +0300

    emre

--- formController/futbolteamForm.py
+++ formController/futbolteamForm.py
@@ -2,11 +2,11 @@ from data import IHAs
 from data import IKAs
 def futbolteamForm():
     #IKA:1
-#    IHA:10
-#IKA     kaleci
-#IHA geri kalan     takım
+    #IHA:10
+    #IKA kaleci
+    #IHA geri kalan takım
     o = 50
-#IHAların x     konumu
+    #IHAların x konumu
     IHAs[0]["x"]=+o + 40
     IHAs[1]["x"]=+o + 60
     IHAs[2]["x"]=+o + 80
@@ -17,7 +17,7 @@ def futbolteamForm():
     IHAs[7]["x"]=+o + 50
     IHAs[8]["x"]=+o + 70
     IHAs[9]["x"]=+o + 90
-#IHAların y konumu
+    #IHAların y konumu
     IHAs[0]["y"]=+ 100
     IHAs[1]["y"]=+ 100
     IHAs[2]["y"]=+ 100
@@ -28,7 +28,7 @@ def futbolteamForm():
     IHAs[7]["y"]=+ 100
     IHAs[8]["y"]=+ 100
     IHAs[9]["y"]=+ 100
-#IHAların z konumu
+    #IHAların z konumu
     IHAs[0]["z"]=+  o + 40
     IHAs[1]["z"]=+  o + 40
     IHAs[2]["z"]=+  o + 40
@@ -41,7 +41,7 @@ def futbolteamForm():
     IHAs[9]["z"]=+  o + 100
     #IKAların x konumu
     IKAs[0]["x"]= +o + 70
-   #IKAların y konumu    
+    #IKAların y konumu    
     IKAs[0]["y"]=+ 0
-   #IKAların z konumu    
+    #IKAların z konumu    
     IKAs[0]["z"] =+o + 20
