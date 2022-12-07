35617555e14e2d00f78f3445f5b615db30cb0d9c
Deniz Karakaş <96745481+denizkarakass@users.noreply.github.com>
2022-12-07 14:37:16 +0300

    m

---
+++ rotationControllers/rotation.py
@@ -0,0 +1,27 @@
+from data import IHAs
+from data import IKAs
+
+
+for i in range(0, IHAs.__len__):
+    if (IHAs[i]["status"] == "1"):
+        while (IHAs[i]["e_x"] < 10):
+            # yukarı çıkma
+            if (IHAs[i]["e_y"] > 10):
+              IHAs[i]["y"] += 10
+             # aşağı inme
+            elif(IHAs[i]["e_y"] < 10):
+              IHAs[i]["y"] -= 10   
+            # sağa gitme
+            if(IHAs[i]["e_z"] > 10):
+              IHAs[i]["z"] += 10  
+            # sola gitme
+            elif(IHAs[i]["e_z"] < 10):
+              IHAs[i]["z"] -= 10    
+            # geri gitme   
+            if(IHAs[i]["e_x"] < 10):
+              IHAs[i]["x"] -= 10
+              
+        while (IHAs[i]["e-y"] < 10):
+            IHAs[i]["x"] += 10
+        while (IHAs[i]["e-z"] < 10):
+            IHAs[i]["x"] += 10
