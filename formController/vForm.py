495a3768a86468626f528fa0a728a8bb8fe04c82
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-08 10:14:37 +0300

    emre

--- formController/vForm.py
+++ formController/vForm.py
@@ -5,34 +5,34 @@ def vForm():
 #IKA:3
    o = 10
 #IHAların x konumu
-   IHAs[0]["x"] =+ o + 10
-   IHAs[1]["x"] =+ o + 20
-   IHAs[2]["x"] =+ o + 30
-   IHAs[3]["x"] =+ o + 40
-   IHAs[4]["x"] =+ o + 50
+   IHAs[0]["x"] += o + 10
+   IHAs[1]["x"] += o + 20
+   IHAs[2]["x"] += o + 30
+   IHAs[3]["x"] += o + 40
+   IHAs[4]["x"] += o + 50
 
 #IHAların y konumu
-   IHAs[0]["y"] =+ o + 100
-   IHAs[1]["y"] =+ o + 100
-   IHAs[2]["y"] =+ o + 100
-   IHAs[3]["y"] =+ o + 100
-   IHAs[4]["y"] =+ o + 100
+   IHAs[0]["y"] += o + 100
+   IHAs[1]["y"] += o + 100
+   IHAs[2]["y"] += o + 100
+   IHAs[3]["y"] += o + 100
+   IHAs[4]["y"] += o + 100
 
 #IHAların z konumu
-   IHAs[0]["z"] =+ o + 30
-   IHAs[1]["z"] =+ o + 20
-   IHAs[2]["z"] =+ o + 10
-   IHAs[3]["z"] =+ o + 20
-   IHAs[4]["z"] =+ o + 30
+   IHAs[0]["z"] += o + 30
+   IHAs[1]["z"] += o + 20
+   IHAs[2]["z"] += o + 10
+   IHAs[3]["z"] += o + 20
+   IHAs[4]["z"] += o + 30
 #IKAların x konumu
-   IKAs[0]["x"] =+ o + 10
-   IKAs[1]["x"] =+ o + 30
-   IKAs[2]["x"] =+ o + 50
+   IKAs[0]["x"] += o + 10
+   IKAs[1]["x"] += o + 30
+   IKAs[2]["x"] += o + 50
 #IKAların y konumu
-   IKAs[0]["y"] =+ o + 0
-   IKAs[1]["y"] =+ o + 0
-   IKAs[2]["y"] =+ o + 0
+   IKAs[0]["y"] += o + 0
+   IKAs[1]["y"] += o + 0
+   IKAs[2]["y"] += o + 0
 #IKAların z konumu
-   IKAs[0]["z"] =+ o + 30
-   IKAs[1]["z"] =+ o + 10
-   IKAs[2]["z"] =+ o + 30
+   IKAs[0]["z"] += o + 30
+   IKAs[1]["z"] += o + 10
+   IKAs[2]["z"] += o + 30
