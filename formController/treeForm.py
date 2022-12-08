d27ab91de77e863382f6a8ee434b9b9d1beab23d
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-08 10:26:34 +0300

    emre

--- formController/treeForm.py
+++ formController/treeForm.py
@@ -7,29 +7,29 @@ def treeForm():
 #IHAlar geri kalanda 
 #IHAların x konumu
     o = 20
-    IHAs[0]["x"] =+  o + 110
-    IHAs[1]["x"] =+  o + 130
-    IHAs[2]["x"] =+  o + 90
-    IHAs[3]["x"] =+  o + 50
-    IHAs[4]["x"] =+  o + 70
+    IHAs[0]["x"] +=  o + 110
+    IHAs[1]["x"] +=  o + 130
+    IHAs[2]["x"] +=  o + 90
+    IHAs[3]["x"] +=  o + 50
+    IHAs[4]["x"] +=  o + 70
 #IHAların y konumu
-    IHAs[0]["y"] =+  100
-    IHAs[1]["y"] =+  100
-    IHAs[2]["y"] =+  100
-    IHAs[3]["y"] =+  100
-    IHAs[4]["y"] =+  100
+    IHAs[0]["y"] +=  100
+    IHAs[1]["y"] +=  100
+    IHAs[2]["y"] +=  100
+    IHAs[3]["y"] +=  100
+    IHAs[4]["y"] +=  100
 #IHAların z konumu
-    IHAs[0]["z"] =+  o + 50  
-    IHAs[1]["z"] =+  o + 50 
-    IHAs[2]["z"] =+  o + 150  
-    IHAs[3]["z"] =+  o + 50
-    IHAs[4]["z"] =+  o + 50
+    IHAs[0]["z"] +=  o + 50  
+    IHAs[1]["z"] +=  o + 50 
+    IHAs[2]["z"] +=  o + 150  
+    IHAs[3]["z"] +=  o + 50
+    IHAs[4]["z"] +=  o + 50
 #IKAların x konumu
-    IKAs[0]["x"] =+  o +70
-    IKAs[1]["x"] =+  o +110
+    IKAs[0]["x"] +=  o +70
+    IKAs[1]["x"] +=  o +110
 #IKAların y konumu
-    IKAs[0]["y"] =+ 0
-    IKAs[1]["y"] =+ 0
+    IKAs[0]["y"] += 0
+    IKAs[1]["y"] += 0
 #IKAların z konumu
-    IKAs[0]["z"] =+  o  
-    IKAs[1]["z"] =+  o 
+    IKAs[0]["z"] +=  o  
+    IKAs[1]["z"] +=  o 
--- formController/triangleStarForm.py
+++ formController/triangleStarForm.py
@@ -6,26 +6,26 @@ from data import     IHAs
 
 
 def triangleStarForm():
-    o =+ 30
-    IHAs[0]["x"] =+ o
-    IHAs[1]["x"] =+ o + 10
-    IHAs[2]["x"] =+ o - 10
-    IHAs[3]["z"] =+ o + 10
-    IHAs[4]["z"] =+ o - 10
-    IHAs[5]["x"] =+ o + 10
-    IHAs[5]["z"] =+ o + 10
-    IHAs[6]["x"] =+ o - 10
-    IHAs[6]["z"] =+ o - 10
-    IHAs[7]["x"] =+ o - 10
-    IHAs[7]["z"] =+ o + 10
-    IHAs[8]["x"] =+ o + 10
-    IHAs[8]["z"] =+ o - 10
+    o += 30
+    IHAs[0]["x"] += o
+    IHAs[1]["x"] += o + 10
+    IHAs[2]["x"] += o - 10
+    IHAs[3]["z"] += o + 10
+    IHAs[4]["z"] += o - 10
+    IHAs[5]["x"] += o + 10
+    IHAs[5]["z"] += o + 10
+    IHAs[6]["x"] += o - 10
+    IHAs[6]["z"] += o - 10
+    IHAs[7]["x"] += o - 10
+    IHAs[7]["z"] += o + 10
+    IHAs[8]["x"] += o + 10
+    IHAs[8]["z"] += o - 10
 
-    IKAs[0]["x"] =+ o
-    IKAs[0]["z"] =+ o + 5
-    IKAs[1]["x"] =+ o + 5
-    IKAs[1]["z"] =+ o
-    IKAs[2]["x"] =+ o - 5
-    IKAs[2]["z"] =+ o
+    IKAs[0]["x"] += o
+    IKAs[0]["z"] += o + 5
+    IKAs[1]["x"] += o + 5
+    IKAs[1]["z"] += o
+    IKAs[2]["x"] += o - 5
+    IKAs[2]["z"] += o
 
 
