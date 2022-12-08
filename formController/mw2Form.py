f89d2dc34666416ac66f6755540ae0c7e278df60
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-08 21:31:49 +0300

    emre

---
+++ formController/mw2Form.py
@@ -0,0 +1,42 @@
+from data import     IHAs
+from data import     IKAs
+def mw2Form():
+#IHA:9
+#IKA:1
+#IHAların x konumu
+    o = 10
+    IHAs[0]["x"] =  o +20
+    IHAs[1]["x"] =  o +20
+    IHAs[2]["x"] =  o +30
+    IHAs[3]["x"] =  o +40
+    IHAs[4]["x"] =  o +40
+    IHAs[5]["x"] =  o +20
+    IHAs[6]["x"] =  o +20
+    IHAs[7]["x"] =  o +30
+    IHAs[8]["x"] =  o +40
+    #IHAların y konumu
+    IHAs[0]["y"] =  50
+    IHAs[1]["y"] =  100
+    IHAs[2]["y"] =  50
+    IHAs[3]["y"] =  100
+    IHAs[4]["y"] =  50
+    IHAs[5]["y"] =  100
+    IHAs[6]["y"] =  50
+    IHAs[7]["y"] =  100
+    IHAs[8]["y"] =  50
+    #IHAların z konumu
+    IHAs[0]["z"] =  o +30
+    IHAs[1]["z"] =  o +10
+    IHAs[2]["z"] =  o +30 
+    IHAs[3]["z"] =  o +10
+    IHAs[4]["z"] =  o +30
+    IHAs[5]["z"] =  o +40
+    IHAs[6]["z"] =  o +60
+    IHAs[7]["z"] =  o +40
+    IHAs[8]["z"] =  o +60
+    #IKAların x konumu
+    IKAs[0]["x"] =  o +40
+    #IKAların y konumu
+    IKAs[0]["y"] = 0
+    #IKAların z konumu
+    IKAs[0]["z"] =  o +40
