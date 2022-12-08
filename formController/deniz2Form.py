1ebcd8d8df2f17935d587b308b168601ba80c86c
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-08 15:19:38 +0300

    emre

---
+++ formController/deniz2Form.py
@@ -0,0 +1,39 @@
+from data import IHAs
+from data import IKAs
+def deniz2Form():
+    #IKA:1
+    #IHA:8
+    o = 50
+    #IHAların x konumu
+    IHAs[0]["x"] = o + 58
+    IHAs[1]["x"] = o + 141
+    IHAs[2]["x"] = o + 107
+    IHAs[3]["x"] = o + 126
+    IHAs[4]["x"] = o + 37
+    IHAs[5]["x"] = o + 160
+    IHAs[6]["x"] = o + 16
+    IHAs[7]["x"] = o + 98
+    #IHAların y konumu
+    IHAs[0]["y"] = 100
+    IHAs[1]["y"] = 100
+    IHAs[2]["y"] = 100
+    IHAs[3]["y"] = 100
+    IHAs[4]["y"] = 100
+    IHAs[5]["y"] = 100
+    IHAs[6]["y"] = 100
+    IHAs[7]["y"] = 100
+    #IHAların z konumu
+    IHAs[0]["z"] = o + 96
+    IHAs[1]["z"] = o + 46
+    IHAs[2]["z"] = o + 69
+    IHAs[3]["z"] = o + 20
+    IHAs[4]["z"] = o + 127
+    IHAs[5]["z"] = o + 188
+    IHAs[6]["z"] = o + 160
+    IHAs[7]["z"] = o + 195
+    #IKAların x konumu
+    IKAs[0]["x"] = o + 158
+    #IKAların y konumu
+    IKAs[0]["y"] = 0
+    #IKAların z konumu
+    IKAs[0]["z"] = o + 63
