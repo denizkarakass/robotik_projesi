e6eccfca8f1a94bfe85c8e9ccdce81b5bdd62d60
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-08 18:37:20 +0300

    emre

---
+++ formController/happyface2Form.py
@@ -0,0 +1,48 @@
+from data import   IHAs
+from data import   IKAs
+def happyface2Form():
+#IKA:2
+#IHA:10
+#IKAlar burnu
+#IHAlar yüzün geri kalanını
+#IHAların x konumu
+         o = 10
+         IHAs[0]["x"] =  o +60
+         IHAs[1]["x"] =  o +160
+         IHAs[2]["x"] =  o +160
+         IHAs[3]["x"] =  o +60
+         IHAs[4]["x"] =  o +85
+         IHAs[5]["x"] =  o +135
+         IHAs[6]["x"] =  o +85
+         IHAs[7]["x"] =  o +85
+         IHAs[8]["x"] =  o +135
+         IHAs[9]["x"] =  o +135
+        #IHAların y konumu
+         IHAs[0]["y"] =  100
+         IHAs[1]["y"] =  100
+         IHAs[2]["y"] =  100
+         IHAs[3]["y"] =  100
+         IHAs[4]["y"] =  100
+         IHAs[5]["y"] =  100
+         IHAs[6]["y"] =  100
+         IHAs[7]["y"] =  100
+        #IHAların z konumu
+         IHAs[0]["z"] =  o  
+         IHAs[1]["z"] =  o  
+         IHAs[2]["z"] =  o +150  
+         IHAs[3]["z"] =  o +150
+         IHAs[4]["z"] =  o +125
+         IHAs[5]["z"] =  o +125
+         IHAs[6]["z"] =  o +50
+         IHAs[7]["z"] =  o +25
+         IHAs[8]["z"] =  o +25 
+         IHAs[9]["z"] =  o +50
+         #IKAların x konumu
+         IKAs[0]["x"] =  o +110
+         IKAs[1]["x"] =  o +100
+        #IKAların y konumu
+         IKAs[0]["y"] = 0
+         IKAs[1]["y"] = 0
+        #IKAların z konumu
+         IKAs[0]["z"] =  o +75
+         IKAs[1]["z"] =  o +75
