52398109a7a7e0f0426ba99bda898fc115dd8d0f
KaraElBatson <210316035@ogr.cbu.edu.tr>
2022-12-08 19:12:36 +0300

    Create mr2Form.py

---
+++ formController/mr2Form.py
@@ -0,0 +1,45 @@
+from data import   IHAs
+from data import   IKAs
+def mr2Form():
+#IKA:2
+#IHA:9
+#IHAların x konumu
+         o = 10
+         IHAs[0]["x"] =  o +10
+         IHAs[1]["x"] =  o +10
+         IHAs[2]["x"] =  o +20
+         IHAs[3]["x"] =  o +30
+         IHAs[4]["x"] =  o +30
+         IHAs[5]["x"] =  o +40
+         IHAs[6]["x"] =  o +50
+         IHAs[7]["x"] =  o +50
+         IHAs[8]["x"] =  o +40
+         #IHAların y konumu
+         IHAs[0]["y"] =  100
+         IHAs[1]["y"] =  150
+         IHAs[2]["y"] =  100
+         IHAs[3]["y"] =  150
+         IHAs[4]["y"] =  100
+         IHAs[5]["y"] =  150
+         IHAs[6]["y"] =  100
+         IHAs[7]["y"] =  150
+         IHAs[8]["y"] =  100
+        #IHAların z konumu
+         IHAs[0]["z"] =  o 
+         IHAs[1]["z"] =  o +40 
+         IHAs[2]["z"] =  o   
+         IHAs[3]["z"] =  o +40
+         IHAs[4]["z"] =  o 
+         IHAs[5]["z"] =  o +40
+         IHAs[6]["z"] =  o +40
+         IHAs[7]["z"] =  o +40
+         IHAs[8]["z"] =  o +20 
+         #IKAların x konumu
+         IKAs[0]["x"] =  o +40
+         IKAs[1]["x"] =  o +50
+        #IKAların y konumu
+         IKAs[0]["y"] = 0
+         IKAs[1]["y"] = 0
+        #IKAların z konumu
+         IKAs[0]["z"] =  o 
+         IKAs[1]["z"] =  o 
